a03639214da7b844344838941f93043e81f93fe7
Amir Modaressanavi <smodare1@jhu.edu>
2023-01-15 21:11:22 -0500

    Home Screen updated

--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,49 @@
-import { Text } from "react-native";
+import { View, Text, FlatList, Pressable, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import ExpoFastImage from 'expo-fast-image';
 
+/*
+Steps:
+1. fetch random image from unsplash
+2. add image to DATA array
+3. render image in FlatList
+*/
+
+const DATA = [
+  {
+    id: "3WXAJndXvCY",
+    img: "https://images.unsplash.com/photo-1672073313545-751c5c93891d?ixid=MnwzOTk0NzZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NzM4MzM5NTA&ixlib=rb-4.0.3",
+    author: "Khaled Ali",
+  }
+]
 function Home() {
+  const renderItem = ({ id, img }) => (
+    <Pressable>
+      <ExpoFastImage
+        uri={img}
+        cachekey={id}
+        style={styles.item}
+        />
+    </Pressable>
+  );
   return (
-    <ExpoFastImage
-      uri="https://cdn-icons-png.flaticon.com/512/919/919851.png"
-      style={{width: 300, height: 300}}
-    />
+    <SafeAreaView>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+        numColumns={2}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  item: {
+    width: 150,
+    height: 150,
+    margin: 10,
+  },
+});
 
 export default Home;
