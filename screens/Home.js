95a4b62885e3bc3bf7015838bce6cd2828dfdcab
Fabiofdez <ffernan9@jh.edu>
2023-01-15 20:46:15 -0500

    image render works

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,12 @@
 import { Text } from "react-native";
+import ExpoFastImage from 'expo-fast-image';
 
 function Home() {
   return (
-    <Text>Home</Text>
+    <ExpoFastImage
+      uri="https://cdn-icons-png.flaticon.com/512/919/919851.png"
+      style={{width: 300, height: 300}}
+    />
   );
 }
 
